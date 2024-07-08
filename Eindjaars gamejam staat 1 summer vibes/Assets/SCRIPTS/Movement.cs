f9df76115395633c435565699a0a1226f31f5612
Canpaii <cansilay70@gmail.com>
2024-07-08 12:19:04 +0200

    euhm idk dingetjes

---
+++ Eindjaars gamejam staat 1 summer vibes/Assets/SCRIPTS/Movement.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private float hor;
+    private float vert;
+    public float speed, jumpHeight, gravity, rayLength;
+    public bool player1, grounded;
+    private Rigidbody rb;
+    private UnityEngine.Vector3 moveDirection;
+    public Transform orientation;
+    public LayerMask whatIsGround;
+    
+
+    bool dashP1;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        Inputs();
+        CheckGround();
+        rb.AddForce(UnityEngine.Vector3.down * gravity, ForceMode.Force);
+    }
+
+    void Inputs()
+    {       
+        if(player1)
+        {
+            hor = Input.GetAxisRaw("HorizontalP1");
+            vert = Input.GetAxisRaw("VerticalP1");
+
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                Jump();
+            }
+        }
+        else
+        {
+            hor = Input.GetAxisRaw("HorizontalP2");
+            vert = Input.GetAxisRaw("VerticalP2");
+
+            if(Input.GetKeyDown(KeyCode.RightShift))
+            {
+                Jump();
+            }
+        }
+        
+        moveDirection = orientation.transform.forward * vert + orientation.transform.right * hor;
+
+        rb.AddForce(moveDirection.normalized * speed * 10, ForceMode.Force);
+    }  
+
+    private void CheckGround()
+    {   
+         Ray ray = new Ray(transform.position, UnityEngine.Vector3.down);
+
+         grounded = Physics.Raycast(ray, rayLength * 0.5f + 0.2f, whatIsGround);
+    }
+
+     private void Jump()
+    {
+        rb.velocity = new UnityEngine.Vector3(rb.velocity.x , 0 , rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpHeight, ForceMode.Impulse);
+    }
+}
