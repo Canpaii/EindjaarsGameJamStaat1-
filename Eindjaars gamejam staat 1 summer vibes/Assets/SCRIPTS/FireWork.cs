49ca4b8ff70b45edeb79fc442367eae8bb82967e
CarrotXX2 <silver.bgame@gmail.com>
2024-07-09 22:27:29 +0200

    FireWorkPush

---
+++ Eindjaars gamejam staat 1 summer vibes/Assets/SCRIPTS/FireWork.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireWork : MonoBehaviour
+{
+    public GameObject prefab;
+    public float minZ;
+    public float maxZ;
+    public float location;
+    public float location2;
+    public Vector3 spawnRotation;
+
+    public void SpawnFireworks(int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            float randomZ = Random.Range(minZ, maxZ);
+            Vector3 spawnPosition = new Vector3(location, location2, randomZ);
+            GameObject spawnedObject = Instantiate(prefab, spawnPosition, Quaternion.Euler(spawnRotation));
+            Destroy(spawnedObject, 3f);
+        }
+    }
+}
--- Eindjaars gamejam staat 1 summer vibes/Assets/SCRIPTS/ScoreCollider.cs
+++ Eindjaars gamejam staat 1 summer vibes/Assets/SCRIPTS/ScoreCollider.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class ScoreCollider : MonoBehaviour
@@ -9,55 +8,58 @@ public class ScoreCollider : MonoBehaviour
     public TMP_Text scoreTextP1, scoreTextP2;
     public bool player1;
     public int scoreToWin;
-
     public GameObject p2Panel, p1Panel;
     public float p1Score, p2Score;
     public BeachBall beachBall;
+    public FireWork fireWork; 
+    public FireWork2 fireWork2; 
+
     public void Start()
     {
         scoreTextP1.text = p1Score.ToString();
         scoreTextP2.text = p2Score.ToString();
     }
 
-   void OnTriggerEnter (Collider other)
-   {    
-        if(other.tag == ("Ball"))
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Ball")
         {
-            if(player1)
+            if (player1)
             {
                 AddPointsP2();
-
                 scoreTextP2.text = p2Score.ToString();
                 beachBall.ResetBeachBallP2();
+                fireWork.SpawnFireworks(15);
             }
             else
             {
                 AddPointsP1();
-
                 scoreTextP1.text = p1Score.ToString();
                 beachBall.ResetBeachBallP1();
+                fireWork2.SpawnFirework2(15);
             }
-        } 
-   }
+        }
+    }
 
-   void AddPointsP1()
-   {
-        p1Score ++;
-        if(p1Score == scoreToWin)
+    void AddPointsP1()
+    {
+        p1Score++;
+        if (p1Score == scoreToWin)
         {
             p1Panel.SetActive(true);
             Time.timeScale = 0;
             Cursor.lockState = CursorLockMode.Confined;
         }
-   }
-   void AddPointsP2()
-   {
-        p2Score ++;
-        if(p2Score == scoreToWin)
+    }
+
+    void AddPointsP2()
+    {
+        p2Score++;
+        if (p2Score == scoreToWin)
         {
             p2Panel.SetActive(true);
             Time.timeScale = 0;
             Cursor.lockState = CursorLockMode.Confined;
         }
-   }
+    }
 }
